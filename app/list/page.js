d87c812a0c9de9b1901a20d2f5c976094d0b9f99
Your Name <your.email@example.com>
2024-02-24 19:14:30 +0900

    commit 내용

---
+++ app/list/page.js
@@ -0,0 +1,10 @@
+
+
+export default function List() {
+
+    return (
+        <div>
+            <h1 className="title">글자수세기</h1>
+        </div>
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+
+  let name = '메모장'
+
+  return (
+    <div>
+      <h1 className="title">{ name }</h1>
+    </div>
+  );
+}
